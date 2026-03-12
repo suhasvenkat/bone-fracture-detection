fb1cefde813adc2d1b79ac0e91467fd8de8b4bbc
Suhas Venkat <suhasvenkat265@gmail.com>
2026-03-12 22:56:37 +0100

    Add GradCAM + fix return values

--- predictions.py
+++ predictions.py
@@ -36,14 +36,13 @@ def preprocess(img):
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name="conv5_block3_out"):
 
     grad_model = tf.keras.models.Model(
-        [model.inputs],
-        [model.get_layer(last_conv_layer_name).output, model.output],
+        inputs=model.inputs,
+        outputs=[model.get_layer(last_conv_layer_name).output, model.output],
     )
 
     with tf.GradientTape() as tape:
-
         conv_outputs, predictions = grad_model(img_array)
-        loss = predictions[:, 0]
+        loss = predictions
 
     grads = tape.gradient(loss, conv_outputs)
 
@@ -54,7 +53,7 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name="conv5_block3_ou
     heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
     heatmap = tf.squeeze(heatmap)
 
-    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.reduce_max(heatmap)
 
     return heatmap.numpy()
 
